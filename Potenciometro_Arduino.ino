1b1044a4655792f590d4b62ac31df3d0f1fb7299
Guilbert <guilbertoliveira@hotmail.com>
2018-10-11 19:21:10 -0300

    Pequena mudança

--- Potenciometro_Arduino.ino
+++ Potenciometro_Arduino.ino
@@ -16,7 +16,6 @@ void loop() {
   valoranalogico = analogRead(A0); // com o analogRead(portaanalogica), conseguimos converter de 0 - 5V em 0 - 1023 (padrao analogico)
   valorpwm = map(valoranalogico, 0, 1023, 0, 255); // a funcao map recebe 5 parametros (valordeentrada, min_entrada, max_entrada, min_saida, max_saida), Remapeia um número de um intervalo para outro
   analogWrite(led, valorpwm); // led (é o numero do pinto que recebera o sinal de saida), valor (é a razao ciclica um valor de 0 a 255), com essa função o arduino ja sabe que é uma saida não necessitando de pinmode
-  Serial.println(valorpwm); // para visualizarmos no console do arduino 
 
 // print no console para entender a diferença 
   Serial.print("valor analogico = " );    // com essa comparação conseguimos perceber a diferença da analogica e digital no console                   
